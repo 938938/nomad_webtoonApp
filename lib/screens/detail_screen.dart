20d8dcb792ae7144e1e3fed593c5bc756c3ae3ab
938938 <938kjh@gmail.com>
2025-12-15 22:01:10 +0900

    [FEAT] 6.13 Futures

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/models/webtoon_detail_model.dart';
+import 'package:untitled/models/webtoon_episode_model.dart';
+import 'package:untitled/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -9,6 +12,21 @@ class DetailScreen extends StatelessWidget {
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +38,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         surfaceTintColor: Colors.transparent,
-        title: Text(title, style: TextStyle(fontSize: 24)),
+        title: Text(widget.title, style: TextStyle(fontSize: 24)),
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -29,7 +47,7 @@ class DetailScreen extends StatelessWidget {
             children: [
               SizedBox(height: 50),
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -48,7 +66,7 @@ class DetailScreen extends StatelessWidget {
                       "User-Agent":
                           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
                     },
-                    thumb,
+                    widget.thumb,
                   ),
                 ),
               ),
